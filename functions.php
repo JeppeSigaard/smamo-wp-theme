21192d2d82d8df3bc6b93c0da4a995f22cdd1f59
JeppeSigaard <jeppe@smatmonkey.dk>
2015-08-13 12:08:31 +0200

    Making a starter theme basic

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php 
+require 'functions/disable-images.php';
+require 'functions/images.php';
+require 'functions/menus.php';
+require 'functions/meta-box.php';
+require 'functions/no-emojis.php';
+require 'functions/post-types.php';
+require 'functions/scripts.php';
---
+++ functions/images.php
@@ -0,0 +1,5 @@
+<?php 
+
+add_theme_support('post-thumbnails');
+add_image_size( 'widescreen', 1600, 900, true );
+add_image_size( 'letterbox', 640, 480, true );
